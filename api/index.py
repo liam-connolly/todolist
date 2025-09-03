d6c2cdcfff00fd36d05bf8121fa2762b3e2e4f4c
Liam Connolly <liam.connolly@us.mcd.com>
2025-09-03 15:36:40 -0500

    front end

---
+++ api/index.py
@@ -0,0 +1,67 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+todos = []
+todos_id_counter = 1
+
+@app.route("/api/todos", methods=["GET"])
+def get_all_todo_items():
+    return todos
+
+@app.route("/api/todos/<int:todo_id>", methods=["GET"])
+def get_todo_items(todo_id):
+    todo = next((todo for todo in todos if todo["id"] == todo_id), None)
+    if todo:
+        return todo
+    return {"error": "Todo item not found"}, 404
+
+
+@app.route("/api/todos", methods=["POST"])
+def create_todo_item():
+    data =  request.get_json()
+    title = data.get("title")
+    if not title:
+        return {"error" : "Title is required"}, 404
+
+    global todos_id_counter
+    todo = {
+        "id": todos_id_counter,
+        "title": title,
+        "complete": False
+    }
+    todos.append(todo)
+    todos_id_counter += 1
+    return todo, 201
+
+@app.route("/api/todos/<int:todo_id>", methods=["PATCH"])
+def update_todo_items(todo_id):
+    data = request.get_json()
+    title = data.get("title")
+    completed = data.get("completed")
+    
+    todo = next((todo for todo in todos if todo["id"] == todo_id), None)
+    if todo:
+        if title is not None:
+            todo["title"] = title
+        if completed is not None:
+            todo["complete"] = completed
+        return todo    
+    return {"error": "Todo item not found"}, 404
+
+@app.route("/api/todos/<int:todo_id>", methods=["DELETE"])
+def delete_todo_item(todo_id):
+    global todos
+    todos = [todos for todo in todos if todo["id"] != todo_id]
+    return {"message": "Todo item deleted"}
+
+@app.route("/api/healthchecker", methods=["GET"])
+def healthchecker():
+    return {"status": "success", "message": "Integrate Flask Framework with Next.js"}
+
+
+
+if __name__ == "__main__":
+    app.run()
